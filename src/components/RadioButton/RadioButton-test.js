a0d9fa678c8378ef90a5e26583667adb752d8b17
avivero <avivero@aviveros-MacBook-Pro.local>
2018-02-07 14:07:34 -0500

    add RadioButton documentation/contract

---
+++ src/components/RadioButton/RadioButton-test.js
@@ -0,0 +1,10 @@
+/*
+Instructions for testing
+
+Find your Component Contract first
+Decide which constraints are worth testing and which aren’t
+Prop types are not worth testing
+Inline styles are usually not worth testing
+The components you render and what props you give them are important to test
+Don’t test things that are not the concern of your component
+*/
